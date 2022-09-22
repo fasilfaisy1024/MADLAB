a7524318f841d5280384bffeee4d82dcf4a686d0
fasilfaisy1024 <fasilfaisy1024@gmail.com>
2022-09-22 11:18:10 +0530

    SimpleCalc

--- SimpleCalc/app/src/main/java/com/example/simplecalc/MainActivity.java
+++ SimpleCalc/app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -5,63 +5,71 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    TextView display_1;
+    TextView display_2;
+
+    Double leftValue;
+    Double rightValue;
+    Double resultValue;
+    String operator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        display_1 = findViewById(R.id.text_dis1);
+        display_2 = findViewById(R.id.text_dis2);
+        leftValue=0.0;
+        rightValue=0.0;
+        resultValue=0.0;
+    }
+    public void getNumber(View view){
+        Button btn = (Button) view;
+        String get_content = display_2.getText().toString();
+        String content = get_content + btn.getText().toString();
+        display_2.setText(content);
+    }
+    public void getSymbol(View view){
+        Button btn = (Button) view;
+        operator = btn.getText().toString();
+        leftValue = Double.parseDouble(display_2.getText().toString());
+        display_2.setText("");
+        display_1.setText(leftValue+operator);
+    }
+    public void doCalculation(View view){
+        rightValue = Double.parseDouble(display_2.getText().toString());
+        switch(operator){
+            case  "+":
+                resultValue = leftValue + rightValue;
+                break;
+            case  "-":
+                resultValue = leftValue - rightValue;
+                break;
+            case  "x":
+                resultValue = leftValue * rightValue;
+                break;
+            case  "/":;
+                resultValue = leftValue / rightValue;
+                break;
+            default:
+                Toast.makeText(this, "Invalid option",Toast.LENGTH_SHORT).show();
+        }
+        display_2.setText(resultValue.toString());
+        display_1.setText(display_1.getText().toString()+rightValue+" =");
     }
 
-    public void calculate(View view) {
-        float num1,num2;
-        float result = 0;
-
-
-
-        EditText res = (EditText) findViewById(R.id.result);
-        EditText e1 = (EditText) findViewById(R.id.num1);
-        EditText e2 = (EditText) findViewById(R.id.num2);
-
-        if (e1.getText().toString().equals("") || e2.getText().toString().equals("")){
-            Toast.makeText(getApplicationContext(),"Invalid Input",Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        num1 = Integer.parseInt(e1.getText().toString());
-        num2 = Integer.parseInt(e2.getText().toString());
-
-        switch (view.getId()) {
-
-            // cases applied over different buttons
-            case R.id.addBtn:
-                result = num1+num2;
-                break;
-
-            case R.id.subBtn:
-                result = num1-num2;
-                break;
-
-            case R.id.mulBtn:
-                result = num1*num2;
-                break;
-
-            case R.id.divBtn:
-                result = num1/num2;
-                break;
-
-            default:
-                Toast.makeText(getApplicationContext(),"Invalid Input",Toast.LENGTH_SHORT).show();
-
-
-        }
-
-        res.setText(result + "");
-
+    public void clearAll(View view){
+        display_2.setText("");
+        display_1.setText("");
+        leftValue=0.0;
+        rightValue=0.0;
+        resultValue=0.0;
+        operator="+";
     }
 }
